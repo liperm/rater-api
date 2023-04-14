b91a14541bae40a26a4fd18b8f4dc1190097568c
Felipe Ribeiro <felipe.ribeiro@hellosaks.com>
2023-04-14 20:15:33 -0300

    refactor: change database structure

--- migration/docker-database-initial.sql
+++ migration/docker-database-initial.sql
@@ -24,19 +24,16 @@ create type rater.item_category as enum(
 create table rater.item(
 	id serial primary key,
 	user_id integer not null,
-	brand_id integer not null,
 	name varchar not null,
 	category rater.item_category not null,
 	average_rating float default 0.0,
+	price float default 0.0,
 	active boolean default true,
 	created_at timestamp default current_timestamp not null,
 	updated_at timestamp default current_timestamp not null,
 	constraint fk_user_id
 		foreign key (user_id)
 			references rater.user(id),
-	constraint fk_brand_id
-		foreign key (brand_id)
-			references rater.brand(id) 
 );
 
 create table rater.review(
@@ -56,12 +53,3 @@ create table rater.review(
 			references rater.item(id)
 );
 
-create table rater.brand(
-    id serial primary key,
-    name varchar not null,
-	average_rating float default 0.0,
-    active boolean default true,
-	profile_picture varchar default 'https://drive.google.com/file/d/10dg-hhQm8jhagSjcgcXbeyo49IHbum5F/view?usp=share_link',
-    created_at timestamp default current_timestamp not null,
-    updated_at timestamp default current_timestamp not null
-);
